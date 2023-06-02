ca3a7bab4ed8b54eea8dd3a96e3518540756531b
Roman Kopylov <romankopilov1712@gmail.com>
2023-06-02 10:27:12 +0300

    changes to comments:
    change classes names;
    change breakpoints

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
